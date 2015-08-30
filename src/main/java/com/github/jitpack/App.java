0308397f1fbf6bddcd7d10742be8dd9e586eb293
JitPack.io <jitpack@jitpack.io>
2015-08-30 10:25:46 +0100

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -6,8 +6,8 @@ package com.github.jitpack;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
