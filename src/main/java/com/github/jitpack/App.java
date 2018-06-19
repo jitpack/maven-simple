302bf184980afe6208e6ebc85ab8624fa41447f3
amithaval7 <amit.haval@gmail.com>
2018-06-19 13:22:33 +0530

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(new App().greet("world"));
+        System.out.println(new App().greet("worldddd"));
     }
 
     public String greet(String name) {
