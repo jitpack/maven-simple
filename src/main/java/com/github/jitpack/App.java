c99cc9dd2a835e473158660d0dc3a89a2797a2f6
Muthusankaranarayanan valliammal <vallir63@gmail.com>
2021-06-26 13:16:36 +0530

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println(new App().greet("world"));
         System.out.println("This is to test the webhook trigger ");
+        System.out.println("Hello, we are testing in the branches");
     }
 
     public String greet(String name) {
