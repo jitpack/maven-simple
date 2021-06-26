0b310d6d34e806fdf125f1f3bf7e6d93c72d85b2
Muthusankaranarayanan valliammal <vallir63@gmail.com>
2021-06-26 13:14:34 +0530

    Update App.java
    For webhook trigger changes

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main(String[] args)
     {
         System.out.println(new App().greet("world"));
+        System.out.println("This is to test the webhook trigger ");
     }
 
     public String greet(String name) {
